be12f6842c9e672f64059dc95e0681e53d6b3d5f
Mann Makwana <aiwithmann@gmail.com>
2025-10-28 09:39:26 +0530

    Refactor TileCoding methods and improve indexing

--- tilecoding.py
+++ tilecoding.py
@@ -9,41 +9,32 @@ class TileCoding:
         self.high = high
         self.nTilings = num_tilings
         self.offset = offset
-        self.I = (self.high - self.low) / self.nTiles
+        self.I = np.divide(np.subtract(self.high,self.low),self.nTiles)
         self.tilings = []
+        self.dim_ranges = []
 
     def constructTilings(self):
         for dim in range(self.low.size):
-            dimTilings = []
+            start_idx = len(self.tilings)
             for tiling_idx in range(self.nTilings):
-                tiling = []
                 l = self.low[dim] + tiling_idx * self.offset[dim]
                 for _ in range(self.nTiles):
-                    tiling.append(np.array([l, l + self.I[dim]]))
+                    self.tilings.append(np.array([l, l + self.I[dim]]))
                     l += self.I[dim]
-                dimTilings.append(tiling)
-            self.tilings.append(dimTilings)
+            self.dim_ranges.append(np.array([start_idx,len(self.tilings)]))
 
-    def getFeature(self, x: np.ndarray):
-        x = np.clip(x, self.low, self.high - 1e-9)
-        features = np.zeros((self.low.size, self.nTilings,self.nTiles))
-        for i,dimension in enumerate(self.tilings):
-            for j,tiling in enumerate(dimension):
-                for k, bound in enumerate(tiling):
-                    if bound[0] <= x[i] < bound[1]:
-                        features[i][j][k] = 1
-        return features
-
-    '''def getFeatureIndices(self, x: float):
+    def getFeatureIndices(self, x: float):
         x = np.clip(x, self.low, self.high - 1e-9)
         indices = []
-        for i, bound in enumerate(self.tilings):
-            if bound[0] <= x < bound[1]:
-                indices.append(i)
-        return indices'''
+        for dim_idx, value in enumerate(x):
+            start,end = self.dim_ranges[dim_idx]
+            for i in range(start,end):
+                if self.tilings[i][0] <= value < self.tilings[i][1]:
+                    indices.append(i)
+        return indices
 
-tc = TileCoding(10,5,np.array([-10,0]),np.array([0,10]),np.array([2,2]))
-print(tc.I)
+#driver code
+tc = TileCoding(5,5,np.array([-10,0]),np.array([0,10]),np.array([2,2]))
 tc.constructTilings()
 print(tc.tilings)
-print(tc.getFeature(np.array([0,5])))
+print(tc.getFeatureIndices(np.array([0,5])))
