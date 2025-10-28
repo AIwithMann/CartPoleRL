e3831c660d4d04d0145f63227c2231806a73bdee
Mann Makwana <aiwithmann@gmail.com>
2025-10-28 08:32:39 +0530

    Refactor TileCoding to support multi-dimensional inputs

--- tilecoding.py
+++ tilecoding.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 class TileCoding:
-    def __init__(self, num_tiles: int, low: float, high: float, num_tilings: int, offset: float) -> None:
+    def __init__(self, num_tiles: int,num_tilings: int, low: np.ndarray, high: np.ndarray, offset: np.ndarray) -> None:
+        if low.size != high.size:
+            raise IndexError("size mismatch between self.low and self.high")
         self.nTiles = num_tiles
         self.low = low
         self.high = high
@@ -11,33 +13,37 @@ class TileCoding:
         self.tilings = []
 
     def constructTilings(self):
-        for tiling_idx in range(self.nTilings):
-            l = self.low + tiling_idx * self.offset
-            for _ in range(self.nTiles):
-                self.tilings.append(np.array([l, l + self.I]))
-                l += self.I
+        for dim in range(self.low.size):
+            dimTilings = []
+            for tiling_idx in range(self.nTilings):
+                tiling = []
+                l = self.low[dim] + tiling_idx * self.offset[dim]
+                for _ in range(self.nTiles):
+                    tiling.append(np.array([l, l + self.I[dim]]))
+                    l += self.I[dim]
+                dimTilings.append(tiling)
+            self.tilings.append(dimTilings)
 
-    def getFeature(self, x: float):
+    def getFeature(self, x: np.ndarray):
         x = np.clip(x, self.low, self.high - 1e-9)
-        features = np.zeros(self.nTiles * self.nTilings)
-        for i, bound in enumerate(self.tilings):
-            if bound[0] <= x < bound[1]:
-                features[i] = 1
+        features = np.zeros((self.low.size, self.nTilings,self.nTiles))
+        for i,dimension in enumerate(self.tilings):
+            for j,tiling in enumerate(dimension):
+                for k, bound in enumerate(tiling):
+                    if bound[0] <= x[i] < bound[1]:
+                        features[i][j][k] = 1
         return features
 
-    def getFeatureIndices(self, x: float):
+    '''def getFeatureIndices(self, x: float):
         x = np.clip(x, self.low, self.high - 1e-9)
         indices = []
         for i, bound in enumerate(self.tilings):
             if bound[0] <= x < bound[1]:
                 indices.append(i)
-        return indices
+        return indices'''
 
-tc = TileCoding(5,0,10,2,1)
+tc = TileCoding(10,5,np.array([-10,0]),np.array([0,10]),np.array([2,2]))
 print(tc.I)
 tc.constructTilings()
 print(tc.tilings)
-print(tc.getFeature(9.5))
-print(tc.getFeature(0.1872))
-print(tc.getFeature(5.66))
-print(tc.getFeatureIndices(6.99))
+print(tc.getFeature(np.array([0,5])))
